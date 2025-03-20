eb45b027137d6d0b961d32a593f71cced892f6a3
matuszekkenealy830 <matuszekkenealy830@gmail.com>
2025-03-20 17:15:34 +0000

    Add 15.go

---
+++ src/15.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int = 5
+    if a > 3 {
+        fmt.Println("a is greater than 3")
+    } else {
+        fmt.Println("a is not greater than 3")
+    }
+}
